12028dde55ae0f0dc3214bb93807b3a5e9073efd
Madhav Sathe <msathe-tech@gmail.com>
2019-12-09 00:35:19 -0500

    update step-3.sh

--- step-3.sh
+++ step-3.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 echo "--kubectl get psp"
 kubectl get psp
-
+echo ""
+echo "--Enter to continue"
+read var
 echo "--kubectl get psp | grep pks | awk '{print $1}'"
 kubectl get psp | grep pks | awk '{print $1}'
 
+echo ""
 echo "--These are out-of-box PSP created by PKS in the Default namespace. We will delete these and create our own in this workshop."
 echo "--Enter to continue."
 read var
@@ -12,6 +15,12 @@ read var
 echo "--kubectl delete psp $(kubectl get psp | grep pks | awk '{print $1}')"
 kubectl delete psp $(kubectl get psp | grep pks | awk '{print $1}')
 
+echo ""
+echo "--Enter to continue"
+read var
+echo "--kubectl get psp | grep pks"
+kubectl get psp | grep pks
+echo ""
 echo "--Enter to continue"
 read var
 
